5663adbf94e0d71359d5cae42066bc1f4a3a46af
Paul Hayes <fofr@users.noreply.github.com>
2014-08-15 19:00:39 +0100

    Move analytics out of template into cached script

--- themes/paulrhayes/source/js/master.js
+++ themes/paulrhayes/source/js/master.js
@@ -67,3 +67,5 @@ function padListLinksWhenFontsLoad($listingLinks, waitThreshold) {
     }, 100);
   }
 }
+
+var _gaq=[['_setAccount','UA-59763-15'],['_trackPageview']];(function(d,t){var g=d.createElement(t),s=d.getElementsByTagName(t)[0];g.async=1;g.src='//www.google-analytics.com/ga.js';s.parentNode.insertBefore(g,s)}(document,'script'));
