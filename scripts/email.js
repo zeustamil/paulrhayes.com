bf896dee0a77e70b6b415d9fa8e3fae86aa2f85d
Paul Hayes <fofr@users.noreply.github.com>
2015-03-14 14:31:05 +0000

    Use `site_email` to avoid printing email in feed

--- scripts/email.js
+++ scripts/email.js
@@ -1,4 +1,4 @@
 /* Usage: {% contact %} */
 hexo.extend.tag.register('contact', function(args, content, options){
-  return '<br /><b>' + hexo.config.email + '</b>';
+  return '<br /><b>' + hexo.config.site_email + '</b>';
 });
